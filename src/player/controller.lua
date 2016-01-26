a6303cb9452b6af8a1c912c1184cd30cb2625e3a
Samarth Wahal <samarthwahal@hotmail.com>
2016-01-26 01:41:11 -0600

    Created a method to define player animations

--- src/player/controller.lua
+++ src/player/controller.lua
@@ -1,11 +1,19 @@
 PlayerController = {}
 
-function PlayerController:init(player)
+function PlayerController.init(player)
     -- load sprite sheet
     player:sheet("assets/spritesheets/players.png")
 
     -- set up grid
     player:grid(31, 31)
+
+    -- create animations
+    defineAnimations(player)
+end
+
+function defineAnimations(player)
+    -- turn animation
+    player:animation("turn", player:frames(1, 1, 2, 1, 3, 1, 4, 1), 0.2)
 end
 
 return PlayerController
